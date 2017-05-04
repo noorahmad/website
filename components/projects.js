f0a697a18c969df6390af467c96f70b6d0874784
noorahmad <noorahmad23@hotmail.com>
2017-05-04 14:53:04 -0700

    fixed structure of projectitem on last commit but forgot to mention it added projects to projects.js

--- components/projects.js
+++ components/projects.js
@@ -4,21 +4,29 @@ import ProjectItem from './projectitem'
 
 const Projects = () =>
   <main className='Projects'>
-    <div>
-      <h1>project</h1>
-    </div>
-    <div>
-      <h2>project item 1</h2>
-      <p>Description</p>
-    </div>
-    <div>
-      <h2>project item 2</h2>
-      <p>Description</p>
-    </div>
-    <div>
-      <h2>project item 3</h2>
-      <p>Description</p>
-    </div>
+    <a name='projects' />
+    <h2>Projects</h2>
+
+    <ProjectItem
+      name='Whats Good Mobile Marketplace'
+      imgSrc={prefixLink('/img/logo-web.png')}
+      imgAlt='Whats Good logo'
+      desc='Whats Good was a mobile marketplace app my group and I decided to make for our senior year software engineering class. It was built with android studio using JavaScript and integrated a database.'
+    />
+
+    <ProjectItem
+      name='Fuel Tracking App'
+      imgSrc={prefixLink('/img/gas.png')}
+      imgAlt='Fuel Logo'
+      desc='Fuel tracking app to track consumption and other things that are done at gas stations.'
+    />
+
+    <ProjectItem
+      name='NachOS'
+      imgSrc={prefixLink('#')}
+      imgAlt='Some icon for nachos placeholder'
+      desc='Built an operating system from a template for OS which I took last summer'
+    />
   </main>
 
 export default Projects
